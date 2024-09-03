80acf7a250857ff5d6090b078bb4fc52919c707d
Pasan Godamune <44201430+pasanjg@users.noreply.github.com>
2024-09-03 13:59:29 +0000

    Update vercel config

--- index.js
+++ index.js
@@ -6,3 +6,5 @@ var port = process.env.PORT || config.port || 9999;
 app.listen(port, null, function (err) {
     console.log('GH OAUTH SERVER, at your service: http://localhost:' + port);
 });
+
+module.exports = app;
